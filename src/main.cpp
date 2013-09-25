5245059e74fa608f9125451013175601a3805d35
sticky-jr <sharayanan@gmail.com>
2013-09-25 13:14:12 +0200

    Added include directory. Some readme info.

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+int main(int argc, char* argv[]) {
+  /**
+  * Program entry point
+  *   @param argc : Command-line argument count
+  *   @param argv : Array of command line arguments
+  *   @returns      A number corresponding to the exit state (0 : no error)
+  */
+
+  return 0;
+}
